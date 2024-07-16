674aa46c333bae0e8f1a058f6d6c7d695133f36f
Aliki Vasili <alikivasili@outlook.com>
2024-07-16 23:06:17 +0300

    Flask app

--- interface/app.py
+++ interface/app.py
@@ -10,10 +10,34 @@ import os
 # Set up the environment variable for the Hugging Face token
 os.environ["HF_TOKEN"] = "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"
 
+# Load model and tokenizer globally to avoid reloading
+model_id_llama2 = "meta-llama/Llama-2-7b-hf"
+model_llama2 = AutoModelForCausalLM.from_pretrained(model_id_llama2)
+tokenizer_llama2 = AutoTokenizer.from_pretrained(model_id_llama2)
+
+# Set up generation config
+generation_config = {
+    "do_sample": True,
+    "temperature": 0.01,
+    "max_new_tokens": 128,
+    "eos_token_id": tokenizer_llama2.pad_token_id,
+    "return_full_text": False,
+}
+
+# Initialize the text generation pipeline
+pipeline_llama2 = pipeline(
+    "text-generation",
+    model=model_llama2,
+    tokenizer=tokenizer_llama2,
+    framework="pt",
+    device=-1,
+)
+
+print("Model loaded!")
+
 # Initialize the Flask app
 app = Flask(__name__)
 CORS(app)  # Enable CORS
-
 
 
 # Load keywords from file
@@ -33,39 +57,19 @@ def find_relevant_article(question, keywords_dict):
 
 # Function to generate response
 def generate_response(question, relevant_article):
-    # Load model and tokenizer globally to avoid reloading
-    model_id_llama2 = "meta-llama/Llama-2-7b-hf"
-    model_llama2 = AutoModelForCausalLM.from_pretrained(model_id_llama2)
-    tokenizer_llama2 = AutoTokenizer.from_pretrained(model_id_llama2)
-
-    # Set up generation config
-    generation_config = {
-        "do_sample": True,
-        "temperature": 0.01,
-        "max_new_tokens": 256,
-        "eos_token_id": tokenizer_llama2.pad_token_id,
-        "return_full_text": False,
-    }
-
-    # Initialize the text generation pipeline
-    pipeline_llama2 = pipeline(
-        "text-generation",
-        model=model_llama2,
-        tokenizer=tokenizer_llama2,
-        framework="pt",
-        device=-1,
-    )
-
+    
     # Template for response generation
     template = """You are an expert in answering questions related to the Deployment of Generic Cross Border eHealth in Cyprus. Given a question and relevant information from the article '{article}', can you answer the question?
     Question: {question}
     Answer:
     """
+    print("Template: " + template)
 
     with open(os.path.join("articles", relevant_article), "r", encoding="utf-8") as f:
         article_text = f.read()
 
     context = template.format(question=question, article=article_text)
+    print("Generating response...")
     response = pipeline_llama2(context)[0]['generated_text']
     return response.strip()
 
@@ -73,13 +77,17 @@ def generate_response(question, relevant_article):
 @app.route('/ask', methods=['POST'])
 def ask():
     user_question = request.json.get('question')
+    print("Question: " + user_question)
     if not user_question:
         return jsonify({"error": "No question provided."}), 400
 
     most_relevant_article = find_relevant_article(user_question, keywords_dict)
+    print("Most Relevant article: " + most_relevant_article)
+    
     response = generate_response(user_question, most_relevant_article)
+    print("Response: " + response)
 
     return jsonify({"response": response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
